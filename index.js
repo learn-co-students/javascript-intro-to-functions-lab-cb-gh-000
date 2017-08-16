de156b94bc6590fa4dea8dfa1cddd9a91f7550f7
Rayana23 <sanzanarayana@gmail.com>
2017-08-17 00:55:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ function logWhisper(string) {
   console.log( string.toLowerCase())
 }
 function sayHiToGrandma(string){
-  if (string.toLowerCase)
+  if (string===string.toLowerCase)
   {
   return "I can\'t hear you!" 
   }
-  if (string.toUpperCase)
+  if (string===string.toUpperCase)
   {
     return "YES INDEED!"
   }
