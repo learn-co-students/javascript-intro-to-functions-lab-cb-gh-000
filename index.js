7a22725336321f5d4cbd92f86e088b3ca5bb56a0
Haoyuan Guo <h62guo@uwaterloo.ca>
2018-01-29 20:41:55 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function logshout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logwhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string === "I love you, Grandma") {
+    return "I love you, too."
+  }
+  else if (string === string.toUpperCase()) {
+    return "Yes INDEED"
+  }
+  else {
+    return "I can't hear you!"
+  }
+}
