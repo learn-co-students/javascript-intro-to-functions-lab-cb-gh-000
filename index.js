af311d6fc030a968c301faed4aef733eb0714d43
Logan Stone <logan.stone20@gmail.com>
2019-08-21 18:57:37 +0000

    Done.

--- index.js
+++ index.js
@@ -10,11 +10,15 @@ function logShout(string) {
   console.log(string.toUpperCase());
 }
 
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
 function sayHiToGrandma(string) {
-  if sting.toUpperCase() === string {
-    return "YES, INDEED!";
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!";
   }
-  else if sting.toLowerCase() === string {
+  else if (string.toLowerCase() === string) {
     return "I can't hear you!";
   } else if (string === "I love you, Grandma.") {
     return "I love you, too.";
