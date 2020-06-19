fe1fe15e8d17e8b1ee595e85ba5c24012e1c87e4
Aj Liam <andrewjordan35@gmail.com>
2020-06-19 15:26:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+function shout(string){
+  return string.toUpperCase();
+}
+
+function whisper(string){
+  return string.toLowerCase();
+}
+
+function sayHiToGrandma("I love you, Grandma"){
+  return string.toLowerCase("I love you, Grandma");
+  return string.toUpperCase("Yes Indeed!");
+  return string.mixedCase("I can\'t hear you!");
+
+}
