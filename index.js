874469d5297dbb8d101d38195cafb75d072a339e
Aj Liam <andrewjordan35@gmail.com>
2020-06-19 15:28:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-function shout(string){
-  return string.toUpperCase();
+function shout(string) {
+  return string.toUpperCase()
 }
 
-function whisper(string){
-  return string.toLowerCase();
+function whisper(string) {
+  return string.toLowerCase()
 }
 
-function sayHiToGrandma("I love you, Grandma"){
-  return string.toLowerCase("I love you, Grandma");
-  return string.toUpperCase("Yes Indeed!");
-  return string.mixedCase("I can\'t hear you!");
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
 
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+
+  return "Are you eating enough?"
 }
